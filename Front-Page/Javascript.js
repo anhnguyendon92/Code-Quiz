d5da72d71d317873fd9f134b7f01fcbbd8664c06
Anh Nguyen <anhnguyendon92@gmail.com>
2020-07-13 10:34:23 -0700

    Finish Javascript

--- Front-Page/Javascript.js
+++ Front-Page/Javascript.js
@@ -1,121 +1,235 @@
-var timeEl = document.getElementById('time');
-var time = 60;
-var timeId;
-var questionEl = document.getElementById('question');
-//var choices = document.getElementById('choices');
-var button = document.getElementById('button');
-var intials = document.getElementById('intials');
-var submit = document.getElementById('submit');
-var index = 0;
-let questionCounter = 0;
+var timeEl = document.getElementById('time'); 
 
+var time = 60; 
 
-let questions = [
-    {
-        question: "What kind of dream car does Anh want to buy one day?",
+var timeId; 
 
-        choices: ["Toyota Supra", "Telsa", "Porsche 911 Turbo", "4Runner"],
-        answer: "Porsche 911 Turbo"
+var questionEl = document.getElementById('question'); 
 
-    },
+var button = document.getElementById('button'); 
 
-    {
-        question: "What was the reason Anh decided to get into tech?",
+var intials = document.getElementById('intials'); 
 
-        choices: ["Money", "Opportunites", "Family", "Fear of being a failure"],
-        answer: "Fear of being a failure"
-    },
+var submit = document.getElementById('submit'); 
 
-    {
-        question: "How many kids does he want one day?",
-        choice1: "0",
-        choice2: "4",
-        choice3: "5",
-        choice4: "8",
-        answer: 3
-    },
+var index = 0; 
 
-    {
-        question: "Where did Anh grow up?",
-        choice1: "Kentucky",
-        choice2: "Seattle",
-        choice3: "Canada",
-        choice4: "Vietnam",
-        answer: 2
-    },
+let questionCounter = 0; 
 
-    {
-        question: "What are his long term goals?",
-        choice1: "Web designer",
-        choice2: "Data analyst",
-        choice3: "Continue with Macadons",
-        choice4: "Software Engineer",
-        answer: 4
-    },
+ 
+ 
+ 
 
-]
-console.log(questions[0].answer, "this is my array")
-var MAX_QUESTION = questions.length;
+let questions = [ 
 
-function startquiz() {
-    var home = document.getElementById('home');
-    home.setAttribute('class', 'hide');
-    questionEl.removeAttribute('class');
-    //Start timer 
-    question()
-}
-function question() {
-    var currentQuestion = questions[questionCounter];
-    console.log(currentQuestion, "the current question");
-    var title = document.getElementById('title');
-    title.textContent = currentQuestion.question
-    choices.innerHTML = ""
-    currentQuestion.choices.forEach(function (choice) {
-        var choicebutton = document.createElement('button');
-        choicebutton.setAttribute('class', 'choice');
-        choicebutton.setAttribute('value', choice);
-        choicebutton.textContent = choice;
-        choicebutton.onclick = questionClick;
-        choices.appendChild(choicebutton)
-    })
-}
-function questionClick() {
-    console.log(this.value, questions[questionCounter].answer);
-    if (this.value === questions[questionCounter].answer) {
-        console.log('answer is correct');
-        questionCounter++; 
-        question();
-    }
-}
+    { 
 
+        question: "What kind of dream car does Anh want to buy one day?", 
 
-button.onclick = startquiz;
+ 
+ 
 
+        choices: ["Toyota Supra", "Telsa", "Porsche 911 Turbo", "4Runner"], 
 
-startGame = () => {
-    score = 0;
-    avaliableQuestions = [...questions];
-    console.log(avaliableQuestions);
-    getNewQuestion();
-};
+        answer: "Porsche 911 Turbo" 
 
-getNewQuestion = () => {
+ 
+ 
 
-    if (avaliableQuestions.length === 0 || questionCounter >= MAX_QUESTION) {
-        //END OF THE PAGE
-        return window.location.assign("./end.html");
-    }
+    }, 
 
-    //questionCounter++;
-    // const questionIndex = Math.floor(Math.random() * avaliableQuestions.length);
-    currentQuestion = avaliableQuestions[questionCounter];
-    question.innerText = currentQuestion.question;
+ 
+ 
 
-};
+    { 
 
-acceptingAnswer = true;
+        question: "What was the reason Anh decided to get into tech?", 
 
+ 
+ 
 
+        choices: ["Money", "Opportunites", "Family", "Fear of being a failure"], 
 
-startGame();
+        answer: "Fear of being a failure" 
 
+    }, 
+
+ 
+ 
+
+    { 
+
+        question: "How many kids does he want one day?", 
+
+         
+
+        choices: ["0", "2", "5", "3"], 
+
+        answer: "3" 
+
+    }, 
+
+ 
+ 
+
+    { 
+
+        question: "Where did Anh grow up?", 
+
+ 
+ 
+
+        choices: ["Vietnam", "Seattle", "Kentucky", "Canada"], 
+
+        answer: "2" 
+
+    }, 
+
+ 
+ 
+
+    { 
+
+        question: "What are his long term goals?", 
+
+         
+
+        choices: ["Web desgin", "Data science", "Macadon", "Software Engineer"], 
+
+        answer: 4 
+
+    }, 
+
+ 
+ 
+
+] 
+
+console.log(questions[0].answer, "this is my array") 
+
+var MAX_QUESTION = questions.length; 
+
+ 
+ 
+
+function startquiz() { 
+
+    var home = document.getElementById('home'); 
+
+    home.setAttribute('class', 'hide'); 
+
+    questionEl.removeAttribute('class'); 
+
+    //Start timer  
+
+    question() 
+
+} 
+
+function question() { 
+
+    var currentQuestion = questions[questionCounter]; 
+
+    console.log(currentQuestion, "the current question"); 
+
+    var title = document.getElementById('title'); 
+
+    title.textContent = currentQuestion.question 
+
+    choices.innerHTML = "" 
+
+    currentQuestion.choices.forEach(function (choice) { 
+
+        var choicebutton = document.createElement('button'); 
+
+        choicebutton.setAttribute('class', 'choice'); 
+
+        choicebutton.setAttribute('value', choice); 
+
+        choicebutton.textContent = choice; 
+
+        choicebutton.onclick = questionClick; 
+
+        choices.appendChild(choicebutton) 
+
+    }) 
+
+} 
+
+function questionClick() { 
+
+    console.log(this.value, questions[questionCounter].answer); 
+
+    if (this.value === questions[questionCounter].answer) { 
+
+        console.log('answer is correct'); 
+
+        questionCounter++;  
+
+        question(); 
+
+    } 
+
+} 
+
+ 
+ 
+ 
+
+button.onclick = startquiz; 
+
+ 
+ 
+ 
+
+startGame = () => { 
+
+    score = 0; 
+
+    avaliableQuestions = [...questions]; 
+
+    console.log(avaliableQuestions); 
+
+    getNewQuestion(); 
+
+}; 
+
+ 
+ 
+
+getNewQuestion = () => { 
+
+ 
+ 
+
+    if (avaliableQuestions.length === 0 || questionCounter >= MAX_QUESTION) { 
+
+        //END OF THE PAGE 
+
+        return window.location.assign("./end.html"); 
+
+    } 
+
+ 
+ 
+
+    currentQuestion = avaliableQuestions[questionCounter]; 
+
+    question.innerText = currentQuestion.question; 
+
+ 
+ 
+
+}; 
+
+ 
+ 
+
+acceptingAnswer = true; 
+
+ 
+ 
+ 
+ 
+
+startGame(); 

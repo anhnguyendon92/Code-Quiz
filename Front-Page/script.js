6f184cee04559d8dc4a36ca3898747661e33116a
Anh Nguyen <anhnguyendon92@gmail.com>
2020-07-15 22:06:47 -0700

    done

--- Front-Page/script.js
+++ Front-Page/script.js
@@ -1,240 +1,204 @@
-function quizGame() {
+function initQuiz() {
+    //  Initialize "time remaining" variable
+        let timeRemaining=0;
+    
+        
+    //  Clicking the "Start Quiz" button starts the quiz, hides the landing container, and displays the quiz container
+        const startButtonEl = document.getElementById("start-button");
+        const timeRemainingEl = document.getElementById("time-remaining");
+        const finalScoreEl = document.getElementById("final-score");
+        const numQuestions = questions.length;
+        const landingContainerEl = document.getElementById("landing-container");
+        const quizContainerEl = document.getElementById("quiz-container");
+        const finalContainerEl = document.getElementById("final-container");
+        const submitButtonEl = document.getElementById("submit-initials");
+        const highscoreButtonEl = document.getElementById("highscore-button");
+        const highscoreContainerEl = document.getElementById("highscore-container");
+        let highScores = [];
+            //  Method to store and retrieve arrays in/from local storage obtained from https://stackoverflow.com/questions/3357553/how-do-i-store-an-array-in-localstorage
+        if (JSON.parse(localStorage.getItem('scores')) !== null) {
+            highScores = JSON.parse(localStorage.getItem("scores"));
+        }
+    
+        function startQuiz() {
+            
+            
+            landingContainerEl.setAttribute("class","container d-none");
+            let rowEl = null;
+            let colEl = null;
+            let headerEl = null;
+            let buttonEl = null;
+            quizContainerEl.setAttribute("class","container");
+            let currentQuestion = 1;
+            let score = 0;
+    //  Upon starting the quiz, the time remaining variable is assigned a value equal to 15 seconds * the number of questions and starts decreasing by 1 each second
+            timeRemaining=numQuestions * 15;
+            timeRemainingEl.setAttribute("value",timeRemaining);
+            //  Method for stopping the interval once it has started obtained from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Timeouts_and_intervals
+            let myInterval = setInterval(function() {
+                if (timeRemaining<1) {
+                    clearInterval(myInterval);
+                    //  When the final question is answered or the timer reaches zero, the quiz container is hidden and the score container is displayed, where the user enters their initials
+                    quizContainerEl.setAttribute("class","container d-none");
+                    finalContainerEl.setAttribute("class","container");
+                    return;
+                }
+                timeRemaining = timeRemaining - 1;
+                timeRemainingEl.setAttribute("value",timeRemaining);
+            },1000);
+            let clickTimeout = false;
+            function generateQuestion(questionNum) {
+                //  During the quiz, the header has the current question, and the answer buttons have the possible answers for that question
+                quizContainerEl.innerHTML = "";
+                rowEl = document.createElement("div");
+                rowEl.setAttribute("class","row");
+                quizContainerEl.append(rowEl);
 
-    var time = 0;
-    var defaultTime = 60;
-    var penaltyTime = 15;
-    var currentQuestion = 0;
-    timeDisplayEl = document.getElementById("time-display")
+                colEl = document.createElement("div");
+                colEl.setAttribute("class","col-0 col-sm-2");
+                rowEl.append(colEl);
 
+                colEl = document.createElement("div");
+                colEl.setAttribute("class","col-12 col-sm-8");
+                rowEl.append(colEl);
 
-    function firstButtons() {
-        document.getElementById("start-btn").addEventListener("click", function () {
-            document.getElementById("main-container").innerHTML = "";
-            currentQuestion = 0;
-            renderQuestion();
-            timer();
-        });
-        document.getElementById("view-highscore-btn").addEventListener("click", function () {
-            handleHighscore();
-        });
-    }
-    firstButtons();
-    // Start a timer and display countdown
-    function timer() {
-        time = defaultTime;
-        mainInterval = setInterval(function () {
-            time = time - 1;
-            timeDisplayEl.innerHTML = time;
-            if (time <= 0) {
-                clearInterval(mainInterval);
-                timeDisplayEl.innerHTML = "00";
-                renderEndGame();
+                colEl = document.createElement("div");
+                colEl.setAttribute("class","col-0 col-sm-2");
+                rowEl.append(colEl);
+
+                colEl = rowEl.children[1];
+                rowEl = document.createElement("div");
+                rowEl.setAttribute("class","row mb-3");
+                colEl.append(rowEl);
+
+                colEl = document.createElement("div");
+                colEl.setAttribute("class","col-12");
+                rowEl.append(colEl);
+
+                headerEl = document.createElement("h2");
+                headerEl.innerHTML = questions[questionNum-1].title;
+                colEl.append(headerEl);
+
+                colEl = quizContainerEl.children[0].children[1];
+                for (let i=0; i<4; i++) {
+                    let rowEl = document.createElement("div");
+                    rowEl.setAttribute("class","row mb-1");
+                    colEl.append(rowEl);
+
+                    let colEl2 = document.createElement("div");
+                    colEl2.setAttribute("class","col-12");
+                    rowEl.append(colEl2);
+
+                    buttonEl = document.createElement("button");
+                    buttonEl.setAttribute("class","btn btn-primary");
+                    buttonEl.setAttribute("type","button");
+                    buttonEl.innerHTML = questions[currentQuestion-1].choices[i];
+                    colEl2.append(buttonEl);
+                    buttonEl.addEventListener("click",function(){
+                        //  When the user clicks one of the answer buttons, if it is the correct answer, the message "Correct" is displayed, and if not, the message "Incorrect" is displayed and 15 seconds deducted from the timer
+                        if (clickTimeout) {
+                            return;
+                        }
+                        clickTimeout = true;
+                        clearInterval(myInterval);
+                        let colEl = quizContainerEl.children[0].children[1];
+                        let rowEl = document.createElement("div");
+                        rowEl.setAttribute("class","row border-top");
+                        colEl.append(rowEl);
+
+                        colEl = document.createElement("div");
+                        colEl.setAttribute("class","col-12");
+                        rowEl.append(colEl);
+
+                        let parEl = document.createElement("p");
+                        colEl.append(parEl);
+                        if (this.innerHTML === questions[currentQuestion - 1].answer) {
+                            parEl.innerHTML = "Correct!";
+                        } else {
+                            parEl.innerHTML = "Incorrect";
+                            timeRemaining = timeRemaining - 15;
+                            if (timeRemaining < 0) {
+                                timeRemaining = 0;
+                            }
+                            timeRemainingEl.setAttribute("value",timeRemaining);
+                        }
+                        currentQuestion++;
+                        if (currentQuestion>questions.length) {
+                            score = timeRemaining;
+                        }
+                        setTimeout(function() {
+                            // When an answer is chosen, pause the timer and show the result for 2 seconds before loading the next question
+                            if (currentQuestion>questions.length) {
+                                // Move to the results page
+                                quizContainerEl.setAttribute("class","container d-none");
+                                finalContainerEl.setAttribute("class","container");
+                                finalScoreEl.setAttribute("value",score);
+                            } else {
+                                generateQuestion(currentQuestion);
+                                clickTimeout = false;
+                                myInterval = setInterval(function() {
+                                    if (timeRemaining<1) {
+                                        clearInterval(myInterval);
+                                        quizContainerEl.setAttribute("class","container d-none");
+                                        finalContainerEl.setAttribute("class","container");
+                                        return;
+                                    }
+                                    timeRemaining = timeRemaining - 1;
+                                    timeRemainingEl.setAttribute("value",timeRemaining);
+                                },1000);
+                            }
+                        },2000);
+                    });
+                }
+                
+
             }
-        }, 1000);
-
-    }
-
-    let containerEl = document.getElementById("main-container");
-
-    function createRow(rowTotal, content) {
-        for (let i = 0; i < rowTotal; i++) {
-            const rowEl = document.createElement("div");
-            rowEl.setAttribute("class", "row")
-            const colEl = document.createElement("div");
-            colEl.setAttribute("class", "col");
-            colEl.append(content);
-            rowEl.append(colEl);
-            containerEl.append(rowEl);
-
-        }
-
-    }
-
-    function renderQuestion() {
-        // Used to clear start button at beginning and clear previous question;
-        containerEl.innerHTML = "";
-        const questionEl = document.createElement("h3");
-        questionEl.innerHTML = questions[currentQuestion].title;
-
-        createRow(1, questionEl);
-
-        let answerEl = "";
-        for (let i = 0; i < questions[currentQuestion].choices.length; i++) {
-
-            answerEl = document.createElement("button");
-            answerEl.setAttribute("class", "btn btn-secondary m-1");
-            answerEl.innerHTML = questions[currentQuestion].choices[i];
-            createRow(1, answerEl)
-
-            answerEl.addEventListener("click", function () {
-                questions[currentQuestion].userAnswer = questions[currentQuestion].choices[i];
-                answerCheck();
-                switchQuestion();
-            })
-        }
-
-    }
-
-    function answerCheck() {
-
-        if (questions[currentQuestion].answer === questions[currentQuestion].userAnswer) {
-            questions[currentQuestion].outcome = true;
-            questions[currentQuestion].time = time;
-            document.getElementById("outcomeDisplay").innerHTML = "Correct!";
-            setTimeout(function () {
-                document.getElementById("outcomeDisplay").innerHTML = "";
-            }, 1500);
-
-        } else {
-            subtractTime()
-            questions[currentQuestion].outcome = false;
-            document.getElementById("outcomeDisplay").innerHTML = "Wrong!";
-            setTimeout(function () {
-                document.getElementById("outcomeDisplay").innerHTML = "";
-            }, 1500);
-
-        }
-    }
-
-    // Decide if time should be subtracted
-    function subtractTime() {
-        // Subtracts
-        time = time - penaltyTime;
-    }
-
-
-
-    function switchQuestion() {
-        if (currentQuestion <= (questions.length - 2)) {
-            currentQuestion = currentQuestion + 1;
-            renderQuestion();
-        } else {
-            time = 0;
-
-        }
-    }
-
-    // Calculate final score
-    function calcFinalScore() {
-        let finalScore = 0;
-        for (let i = 0; i < questions.length; i++) {
-            if (questions[i].outcome) {
-                finalScore = finalScore + questions[i].time;
+            function saveHighScore() {
+                let initialsEl = document.getElementById("initials-entry");
+                let newHighScore = {
+                    initials: initialsEl.value,
+                    highScore: score
+                };
+                console.log(newHighScore);
+                highScores.push(newHighScore);
+                console.log(highScores);
+                localStorage.setItem("scores",JSON.stringify(highScores));
             }
-            else {
-
-            }
+            submitButtonEl.addEventListener("click",saveHighScore);
+            
+            generateQuestion(currentQuestion);
         }
-        return finalScore;
-    }
 
-    function renderEndGame() {
+        startButtonEl.addEventListener("click",startQuiz);
 
-        containerEl.innerHTML = "";
+        highscoreButtonEl.addEventListener("click",function() {
+            landingContainerEl.setAttribute("class","container d-none");
+            quizContainerEl.setAttribute("class","container d-none");
+            finalContainerEl.setAttribute("class","container d-none");
+            highscoreContainerEl.setAttribute("class","container");
+            let colEl = document.getElementById("highscore-table");
+            for (i=0; i<highScores.length; i++) {
+                let rowEl = document.createElement("div");
+                rowEl.setAttribute("class","row mb-1");
+                colEl.append(rowEl);
 
+                let colEl2 = document.createElement("div");
+                colEl2.setAttribute("class","col-12 text-center");
+                rowEl.append(colEl2);
 
-        const endGameMessageEl = document.createElement('div');
-        endGameMessageEl.setAttribute('class', 'display-3');
-        endGameMessageEl.innerText = "All done!";
-
-
-        const userScoreMessageEl = document.createElement('h4');
-        userScoreMessageEl.innerHTML = "Your score was: " + calcFinalScore();
-        endGameMessageEl.append(userScoreMessageEl);
-
-
-        const initialMessageEl = document.createElement('div');
-        initialMessageEl.setAttribute('class', 'user-input');
-        initialMessageEl.innerHTML = "Enter your intials: <input type='text' id='initial-input'></input>"
-        endGameMessageEl.append(initialMessageEl);
-
-
-        const addHighScoreBtnEl = document.createElement('button');
-        addHighScoreBtnEl.setAttribute('class', 'btn btn-success');
-        addHighScoreBtnEl.setAttribute('id', 'submit-btn');
-        addHighScoreBtnEl.innerText = "Submit Highscore";
-        endGameMessageEl.append(addHighScoreBtnEl);
-
-
-        createRow(1, endGameMessageEl);
-
-        addHighScoreBtnEl.addEventListener("click", function () {
-
-            let highscores = [];
-            if (localStorage.getItem('localHighscores')) {
-                highscores = localStorage.getItem('localHighscores');
-                https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
-                highscores = JSON.parse(highscores);
-            } else {
-                let highscores = [];
+                let parEl = document.createElement("div");
+                parEl.innerHTML = "Initials: " + highScores[i].initials + "   Score: " + highScores[i].highScore;
+                colEl2.append(parEl);
             }
-            const userInitial = document.getElementById('initial-input').value;
-            const userScore = calcFinalScore();
-            highscores[(highscores.length)] = {
-                initial: userInitial,
-                score: userScore
-            }
-            // Sorts highscores based on the best score in the array. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-            highscores.sort(function (a, b) {
-                return b.score - a.score;
-            })
-
-            // Got code for JSON.stringify at https://blog.logrocket.com/the-complete-guide-to-using-localstorage-in-javascript-apps-ba44edb53a36/
-            window.localStorage.setItem('localHighscores', JSON.stringify(highscores));
-
-            handleHighscore(highscores);
         });
+    
+        
+    
+    
+    
+    
+    
+    
     }
-
-    // Make code restart and be able to erase highscores
-
-    function handleHighscore(highscores) {
-
-        if (localStorage.getItem('localHighscores')) {
-            highscores = localStorage.getItem('localHighscores');
-            https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
-            highscores = JSON.parse(highscores);
-        } else {
-            highscores = [];
-        }
-        document.body.innerHTML = "";
-        const highscoreContainerEl = document.createElement('div');
-        highscoreContainerEl.setAttribute('class', 'container');
-
-        // title for highscore page
-        const highscoreTitleEl = document.createElement('div');
-        highscoreTitleEl.setAttribute('class', 'display-2 text-center mb-3')
-        highscoreTitleEl.innerHTML = "Highscores";
-        highscoreContainerEl.append(highscoreTitleEl);
-
-        // element for each highscore and appends them to the container.
-        for (let i = 0; i < highscores.length; i++) {
-            let highscoreDisplayEl = document.createElement('div');
-            highscoreDisplayEl.setAttribute('class', 'm-1 bg-secondary text-white p-1')
-            highscoreDisplayEl.innerText = (i + 1) + ". " + highscores[i].initial + " - " + highscores[i].score;
-            highscoreContainerEl.append(highscoreDisplayEl);
-        }
-
-        //restart button
-        restartBtnEl = document.createElement('button');
-        restartBtnEl.setAttribute('class', 'btn btn-success m-1');
-        restartBtnEl.innerText = 'Restart Quiz';
-        highscoreContainerEl.append(restartBtnEl);
-        restartBtnEl.addEventListener('click', function () {
-            document.location.reload()
-        });
-        //Creates clear highscores button
-        clearScoresBtnEl = document.createElement('button');
-        clearScoresBtnEl.setAttribute('class', 'btn btn-danger m-1');
-        clearScoresBtnEl.innerText = 'Clear Highscores';
-        highscoreContainerEl.append(clearScoresBtnEl);
-        clearScoresBtnEl.addEventListener('click', function () {
-            window.localStorage.removeItem('localHighscores');
-            handleHighscore();
-        });
-        // Appends highscore content to body so it is viewable.
-        document.body.append(highscoreContainerEl);
-
-    }
-}
-quizGame()
+    
+initQuiz();
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Code-Quiz
+
+Once you click Start Quiz arrays of question pop up 
+
+Index Page features high school and game button link
+
+A timer with a penalty feature is included as well. Every time the user gets the answer correct, it'll go to the next question. But if incorrect -15 seconds off the timer.
+
+*Saves highscore in local storage
